79347d964e02ba87b81fca066ba3e0a047022df6
german vertel <gvertel88@gmail.com>
2024-06-26 13:58:59 -0500

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by German David Vertel Narvaez on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by German David Vertel Narvaez on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
